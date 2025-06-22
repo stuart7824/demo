4233d5cdda65efd1a74e061e4814f922c7cecb94
stuart7824 <stuartcerejo31@gmail.com>
2025-06-22 09:32:00 +0530

    datatypes in C++

---
+++ c++_datatypes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main() {
+    
+    // integer (whole number)
+    int age = 12;
+    int days = 87;
+    int year = 2025;
+
+    // float (number with 6-7 decimal places)
+    double price = 53.65;
+    double gpa = 67.76;
+    double temperature = 39.5;
+
+    // double (number with 15 decimal places)
+    double price = 53.65;
+    double gpa = 67.76;
+    double temperature = 39.5;
+
+    // char (single character)
+    char grade = 'A';
+    char inital = 'N';
+    char currency = '$';
+
+    // boolean (true/false)
+    bool student = true;
+    bool power = false;
+    bool forsale = true;
+
+    // string (sequnce of text)
+    std::string name = "stuart";
+    std::string day = "friday";
+    std::string food = "tossin pizza"
+
+    return 0;
+}
